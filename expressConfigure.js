26f51a5b067ee822becb279f5550439b9375ab16
John McLear <john@mclear.co.uk>
2020-04-27 21:12:01 +0000

    working csp

--- expressConfigure.js
+++ expressConfigure.js
@@ -1,5 +1,12 @@
 const helmet = require('helmet');
 const settings = require('../../src/node/utils/Settings');
+const crypto = require('crypto');
+/*
+app.use((req, res, next) => {
+  res.locals.nonce = uuid.v4()
+  next()
+})
+*/
 
 exports.expressConfigure = function(hookName, app){
 
@@ -7,15 +14,23 @@ exports.expressConfigure = function(hookName, app){
   // Check settings are set..
   if(settings.ep_helmet && settings.ep_helmet.csp){
     const csp = require('helmet-csp')
-    var scriptSrc = [
-      "'unsafe-eval'",
-      "'self'",
-      "'sha256-ibNaIvmfNwDgwoWLRuzNqZ70N73f+h1vy5W+CIb0mYs='",
-      "'sha256-428+JzVAjCsMgZnl45z9QF/vl/gV8cYmw6ECAomwbzg='",
-      "'sha256-Ka9+GFLL5C2nb5noIeyZLrMf5dINEkPE/vvE5p0gwSc='",
-      "'sha256-BdOZlu8ThnsrDyvG8O5phcbXQQfsf6sIF7j8NZ7PpKw='",
-      "'sha256-KZVi2ucSvzF5CKcaBx4eZYxBx361dQPNVRuIHrN3zsA='"
-    ]
+    settings.nonce = {};
+    // each page, this needs adding to if someone adds more inline JS
+    var noncedPages = [
+      'indexLicense', 'index',
+      'padLicense', 'padErrorHandler', 'padBootstrap', 'importFrame',
+      'timesliderLicense', 'timesliderBootstrap'
+    ];
+    var scriptSrc = []; // array of each nonce
+    scriptSrc.push("'unsafe-eval'");
+    scriptSrc.push("'self'");
+    scriptSrc.push("'nonce-aceTemporaryNonce'");
+    noncedPages.forEach(function(item){
+      let nonce = crypto.randomBytes(8).toString('base64'); // generate the nonce
+      scriptSrc.push(`\'nonce-${nonce}\'`); // push the nonces to the array
+      settings.nonce[item] = nonce; // apply the nonce to the setting object...
+    });
+
     if(settings.ep_helmet.csp.directives.scriptSrc){
       settings.ep_helmet.csp.directives.scriptSrc = scriptSrc;
     }
