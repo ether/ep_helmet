889bcc3e3eeb2cce4d19763efd3cdc58a05031d1
John McLear <john@mclear.co.uk>
2020-11-22 16:35:09 +0000

    autofixes from Etherpad checkPlugins.js

--- expressConfigure.js
+++ expressConfigure.js
@@ -8,42 +8,46 @@ app.use((req, res, next) => {
 })
 */
 
-exports.expressConfigure = function(hookName, app){
+exports.expressConfigure = function (hookName, app) {
   app.app.use(helmet());
 
-  if(settings.ep_helmet){
-    console.debug("Using config from Helmet. ", settings.ep_helmet);
+  if (settings.ep_helmet) {
+    console.debug('Using config from Helmet. ', settings.ep_helmet);
   }
 
   // Check settings are set..
-  if(settings.ep_helmet && settings.ep_helmet.csp){
-    const csp = require('helmet-csp')
+  if (settings.ep_helmet && settings.ep_helmet.csp) {
+    const csp = require('helmet-csp');
     settings.nonce = {};
     // each page, this needs adding to if someone adds more inline JS
-    var noncedPages = [
-      'indexLicense', 'index',
-      'padLicense', 'padErrorHandler', 'padBootstrap', 'importFrame',
-      'timesliderLicense', 'timesliderBootstrap'
+    const noncedPages = [
+      'indexLicense',
+      'index',
+      'padLicense',
+      'padErrorHandler',
+      'padBootstrap',
+      'importFrame',
+      'timesliderLicense',
+      'timesliderBootstrap',
     ];
-    var scriptSrc = []; // array of each nonce
+    const scriptSrc = []; // array of each nonce
     scriptSrc.push("'unsafe-eval'");
     scriptSrc.push("'self'");
     scriptSrc.push("'nonce-aceTemporaryNonce'");
-    noncedPages.forEach(function(item){
-      let nonce = crypto.randomBytes(8).toString('base64'); // generate the nonce
+    noncedPages.forEach((item) => {
+      const nonce = crypto.randomBytes(8).toString('base64'); // generate the nonce
       scriptSrc.push(`\'nonce-${nonce}\'`); // push the nonces to the array
       settings.nonce[item] = nonce; // apply the nonce to the setting object...
     });
 
-    if(settings.ep_helmet.csp.directives.scriptSrc){
+    if (settings.ep_helmet.csp.directives.scriptSrc) {
       settings.ep_helmet.csp.directives.scriptSrc = scriptSrc;
     }
-    app.app.use(csp(settings.ep_helmet.csp))
+    app.app.use(csp(settings.ep_helmet.csp));
   }
 
   // Frameguard is useful for x-frame-origin
-  if(settings.ep_helmet && settings.ep_helmet.frameguard){
+  if (settings.ep_helmet && settings.ep_helmet.frameguard) {
     app.app.use(helmet.frameguard(settings.ep_helmet.frameguard));
   }
-  
-}
+};
